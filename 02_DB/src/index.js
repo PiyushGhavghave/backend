27d1825fabedb5f839cac538ad2db49a7b383fee
PiyushGhavghave <piyushghavghave@gmail.com>
2024-12-13 18:06:40 +0530

    added middelwares (cors, cookie parser) and express methods

--- 02_DB/src/index.js
+++ 02_DB/src/index.js
@@ -3,6 +3,16 @@ dotenv.config({
     path : './.env'
 })
 import connectDB from "./db/connect_DB.js";
+import {app} from './app.js';
+const port = process.env.PORT || 8000;
 
 connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Appliaction is sreving on port ${port}`);
+    })
+})
+.catch((err) => {
+    console.log("Mongodb Connection failed : ", err);
+})
 
